38eaa345ed529db51a8c38465013c1c3adcfe302
redmasterlovecat28-art <302406597+Rewwy288@users.noreply.github.com>
2026-08-29 01:48:01 +0700

    d

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,4 +3,27 @@ using UnityEngine;
 public static class Settings
 {
     public static bool fromSave;
+}using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        if (AudioManager.instance != null)
+        {
+            // ดึงค่าที่เคยเซฟไว้มาตั้งให้ Slider ขยับไปตรงนั้น
+            slider.value = AudioManager.instance.LoadCurrentMasterVol();
+
+            // ผูก Event ให้เวลากดเลื่อน Slider จะไปเรียกใช้ AdjustMasterVolume ใน AudioManager
+            slider.onValueChanged.AddListener(AudioManager.instance.AdjustMasterVolume);
+        }
+    }
 }
