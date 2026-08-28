4963508504206f6c4647d60c2494007e587b78b9
redmasterlovecat28-art <302406597+Rewwy288@users.noreply.github.com>
2026-08-28 17:32:34 +0700

    ty

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public enum BallColor
+{
+    White,
+    Red,
+    Green,
+    Yellow,
+    Brown,
+    Blue,
+    Black,
+    Pink
+}
+
+public class Ball : MonoBehaviour, IPointerClickHandler
+
+{
+    [SerializeField]
+    private int point;
+    public int Point { get { return point; } set { point = value; } }
+
+    [SerializeField]
+    private BallColor color;
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    [SerializeField]
+    private GameManager gameManager; // drag reference in inspector or assign in code
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(point);
+        Destroy(gameObject);
+        gameManager.PlayerScore += point; // instance member on the referenced object
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SetColorandPoint(BallColor color)
+    {
+
+        switch (color)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Green:
+                point = 2;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Yellow:
+                point = 3;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Black:
+                point = 6;
+                rd.material.color = Color.black;
+                break;
+            case BallColor.Pink:
+                point = 7;
+                rd.material.color = Color.pink;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,4 @@
+﻿internal class GameManager
+{
+    public int PlayerScore { get; internal set; }
+}
